ace047f6447e937a2ba7726ac70f4485463ef70e
jitingcat <jitingchina@gmail.com>
2017-11-08 23:26:28 +0800

    test pow

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double a;
+    double b;
+    double c;
+    a = 4;
+    b = 4;
+    c = a * pow(10,b);
+    cout << c;
+    return 0;
+}
