9db41fc81787141df38d50018aedbc0f6f024a1e
jitingcat <jitingchina@gmail.com>
2017-11-22 16:31:23 +0800

    test pow

--- main.cpp
+++ main.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
 int main() {
-    double a;
-    double b;
-    double c;
-    a = 4;
-    b = 4;
-    c = a * pow(10,b);
-    cout << c;
+    cout << "Hello world!";
     return 0;
 }
